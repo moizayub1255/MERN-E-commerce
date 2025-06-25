52a5f2cd00d7c70a6b82dc2cb7fc8faf16e69438
Moiz <moizayub401@gmail.com>
2025-06-25 20:32:15 +0500

    done

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -109,7 +109,7 @@ const CartPage = () => {
                 >
                   <div className="col-md-4">
                     <img
-                      src={`/api/v1/product/product-photo/${p._id}`}
+                      src={`${process.env.REACT_APP_API_URL}/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="150%"
--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -48,7 +48,7 @@ const ProductDetails = () => {
       <div className="row container product-details">
         <div className="col-md-6">
           <img
-            src={`/api/v1/product/product-photo/${product._id}`}
+            src={`${process.env.REACT_APP_API_URL}/api/v1/product/product-photo/${product._id}`}
             className="card-img-top img-fluid"
             alt={product.name}
             // height="300"
@@ -102,7 +102,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p._id}`}
+                src={`${process.env.REACT_APP_API_URL}/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
